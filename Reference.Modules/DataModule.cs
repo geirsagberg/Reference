2753af0bc082e0106f6de0d4608c0cc00410519a
Geir Sagberg <geir.sagberg@gmail.com>
2015-09-30 23:04:37 +0200

    Add ModuleConfig

---
+++ Reference.Modules/DataModule.cs
@@ -0,0 +1,14 @@
+﻿using Reference.Common.Contracts;
+using Reference.Data;
+using Reference.Modules.LightInject;
+
+namespace Reference.Modules
+{
+    internal class DataModule : ICompositionRoot
+    {
+        public void Compose(IServiceRegistry serviceRegistry)
+        {
+            serviceRegistry.Register<IEntityContext, EntityContext>();
+        }
+    }
+}
---
+++ Reference.Modules/ModuleConfig.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Reflection;
+using System.Web.Http;
+using Reference.Modules.LightInject;
+
+namespace Reference.Modules
+{
+    public class ModuleConfig
+    {
+        private static readonly Lazy<ServiceContainer> _container = new Lazy<ServiceContainer>(() => {
+            var container = new ServiceContainer();
+            RegisterTypes(container);
+            return container;
+        });
+
+        private static ServiceContainer Container => _container.Value;
+
+        private static void RegisterTypes(ServiceContainer container)
+        {
+            // Registers all ICompostionRoot implementations in this assembly
+            container.AssemblyScanner.Scan(Assembly.GetExecutingAssembly(), container);
+        }
+
+        public static void EnableMvc()
+        {
+            Container.RegisterControllers(Assembly.GetCallingAssembly());
+            Container.EnableMvc();
+        }
+
+        public static void EnableWebApi(HttpConfiguration config)
+        {
+            Container.RegisterApiControllers(Assembly.GetCallingAssembly());
+            Container.EnablePerWebRequestScope();
+            Container.EnableWebApi(config);
+        }
+
+        public static void RegisterPerContainerLifetime<T1, T2>()
+        {
+            Container.Register(typeof (T1), typeof (T2), new PerContainerLifetime());
+        }
+
+        public static void RegisterPerContainerLifetime<T>()
+        {
+            Container.Register(typeof (T), new PerContainerLifetime());
+        }
+
+        public static T GetInstance<T>() => Container.GetInstance<T>();
+
+        public static void RegisterInstance<T>(object instance)
+        {
+            Container.RegisterInstance(typeof (T), instance);
+        }
+    }
+}
