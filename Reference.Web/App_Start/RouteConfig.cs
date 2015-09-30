727c6b6b27b86788dd058ec8536d0b0591ec7f02
Geir Sagberg <geir.sagberg@gmail.com>
2015-09-30 23:20:43 +0200

    Started on MVC

---
+++ Reference.Web/App_Start/RouteConfig.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Reference.Web
+{
+    public static class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "Error - 404",
+                "NotFound",
+                new { controller = "Error", action = "NotFound" });
+
+            routes.MapRoute(
+                "Error - 500",
+                "ServerError",
+                new { controller = "Error", action = "ServerError" });
+
+            routes.MapMvcAttributeRoutes();
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
